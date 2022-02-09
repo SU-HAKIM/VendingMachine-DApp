6b67818c1dd253ab06e40a4391daeec373503813
sadiul-hakim <bmsuhakim@gmail.com>
2022-02-09 16:05:43 +0600

    vmContract ready

---
+++ vending_machine-ui/blockchain/vending.js
@@ -0,0 +1,13 @@
+import Web3 from 'web3';
+
+const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/v3/e3c1c48968cd492d9e9faad0d4bda502'
+);
+
+const web3 = new Web3(provider);
+
+const abi = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "donutBalances", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getVendingMachineBalance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "purchase", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "restock", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]
+
+const vmContract = new web3.eth.Contract(abi, "0xD079636842a720FF9e9B15cF1E0a924a6721F800");
+
+export default vmContract;
